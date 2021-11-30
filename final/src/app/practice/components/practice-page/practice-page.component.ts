1b56e895128a67bb40f7e7d1a40d52960356d70c
Sergei Rusakov <sergei_rusakov@epam.com>
2021-11-30 13:26:36 +0500

    chore: add skull

--- final/src/app/practice/components/practice-page/practice-page.component.ts
+++ final/src/app/practice/components/practice-page/practice-page.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, NgZone, Renderer2, ViewChild } from '@angular/core';
 import * as Three from 'three';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 @Component({
   selector: 'app-practice-page',
@@ -9,17 +10,34 @@ import * as Three from 'three';
 export class PracticePageComponent implements AfterViewInit {
 
   @ViewChild('pageElem') pageElem: ElementRef<HTMLDivElement> | undefined;
-  scene: Three.Scene;
-  camera: Three.PerspectiveCamera;
-  threeRenderer: Three.Renderer;
-  cube: Three.Mesh | undefined;
+  scene!: Three.Scene;
+  camera!: Three.PerspectiveCamera;
+  threeRenderer!: Three.Renderer;
+  loader = new GLTFLoader();
+
+  model: Three.Object3D | undefined;
 
   constructor(
     private renderer: Renderer2,
     private zone: NgZone,
   ) {
+    this.initScene();
+    this.initCamera();
+    this.initRenderer();
+  }
+
+  initScene(): void {
     this.scene = new Three.Scene();
+    this.scene.background = new Three.Color('white');
+    const light = new Three.AmbientLight( 0x404040, 5 ); // soft white light
+    this.scene.add( light );
+  }
+
+  initCamera(): void {
     this.camera = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  }
+
+  initRenderer(): void {
     this.threeRenderer = new Three.WebGLRenderer();
     this.threeRenderer.setSize(window.innerWidth, window.innerHeight);
   }
@@ -27,28 +45,28 @@ export class PracticePageComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     if (this.pageElem) {
       this.renderer.appendChild(this.pageElem.nativeElement, this.threeRenderer.domElement);
-      this.renderCube();
+      this.renderSkull();
       this.zone.runOutsideAngular(() => {
         this.animate();
       });
     }
   }
 
-  renderCube(): void {
-    const geometry = new Three.BoxGeometry();
-    const material = new Three.MeshBasicMaterial( { color: 0x00ff00 } );
-    this.cube = new Three.Mesh( geometry, material );
-    this.scene.add(this.cube);
-
-    this.camera.position.z = 5;
+  renderSkull(): void {
+    this.loader.load('assets/skull/scene.gltf', (scene) => {
+      this.model = scene.scene.children[0];
+      this.camera.position.z = 5;
+      this.scene.add(this.model);
+    }, undefined, (error) => {
+      console.log(error);
+    });
   }
 
   animate(): void {
     requestAnimationFrame(this.animate.bind(this));
 
-    if (this.cube) {
-      this.cube.rotation.x += 0.01;
-      this.cube.rotation.y += 0.01;
+    if (this.model) {
+      this.model.rotation.z += 0.01;
     }
 
     this.threeRenderer.render(this.scene, this.camera);
